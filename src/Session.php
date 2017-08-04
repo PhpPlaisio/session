5957755128c950375a6e9d908662a8fe0e36b1cd
Paul Water <p.r.water@setbased.nl>
2017-08-04 22:45:46 +0200

    Initial commit.

---
+++ src/Session.php
@@ -0,0 +1,119 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Abc\Session;
+
+/**
+ * Interface for classes for session handling.
+ */
+interface Session
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the ID of company of the current session.
+   *
+   * @return int
+   */
+  public function getCmpId();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns stateful double submit token to prevent CSRF attacks.
+   *
+   * @return string
+   */
+  public function getCsrfToken();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the code of preferred language of the user of the current session.
+   *
+   * @return string
+   */
+  public function getLanCode();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the ID of preferred language of the user of the current session.
+   *
+   * @return int
+   */
+  public function getLanId();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the ID of the profile of the user of the current session.
+   *
+   * @return int
+   */
+  public function getProId();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the ID of the current session.
+   *
+   * @return int|null
+   */
+  public function getSesId();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the session token.
+   *
+   * @return string
+   */
+  public function getSessionToken();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the ID of the user of the current session.
+   *
+   * @return int
+   */
+  public function getUsrId();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns true if the user is anonymous (i.e. not a user who has logged in). Otherwise, returns false.
+   *
+   * @return bool
+   */
+  public function isAnonymous();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Updates the session that an user has successfully logged in.
+   *
+   * @param int $usrId The ID the user.
+   *
+   * @return void
+   */
+  public function login($usrId);
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Terminates the current session.
+   *
+   * @return void
+   */
+  public function logout();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Saves the current state op the session.
+   *
+   * @return void
+   */
+  public function save();
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Creates a session or resumes the current session based on the session cookie.
+   *
+   * @return void
+   */
+  public function start();
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
