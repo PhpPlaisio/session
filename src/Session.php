09b17bedd747e30fcaa6477b9f5c5538d6a61792
Paul Water <p.r.water@setbased.nl>
2017-12-03 13:40:48 +0100

    Removed depreciated method getCmpId (Abc::$companyResolver->getCmpId() must be used instead).

--- src/Session.php
+++ src/Session.php
@@ -9,16 +9,6 @@ interface Session
 {
   //--------------------------------------------------------------------------------------------------------------------
   /**
-   * Returns the ID of company of the current session.
-   *
-   * @return int
-   *
-   * @deprecated Use Abc::$companyResolver->getCmpId() instead.
-   */
-  public function getCmpId();
-
-  //--------------------------------------------------------------------------------------------------------------------
-  /**
    * Returns stateful double submit token to prevent CSRF attacks.
    *
    * @return string
