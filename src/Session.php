0f6471633c908e1b2bd1807eb1b0d62a476a0b7d
Paul Water <p.r.water@setbased.nl>
2018-06-29 09:40:50 +0200

    Added named section methods.

--- src/Session.php
+++ src/Session.php
@@ -9,6 +9,30 @@ interface Session
 {
   //--------------------------------------------------------------------------------------------------------------------
   /**
+   * Exclusive lock mode for named section of a session.
+   */
+  const NAMED_LOCKED = 1;
+
+  /**
+   * Shared lock mode for named section of a session.
+   */
+  const NAMED_SHARED = 2;
+
+  /**
+   * First come, first serve mode for named section of a session.
+   */
+  const NAMED_FIRST_COME_FIRST_SERVED = 3;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Deletes a named section of the session.
+   *
+   * @param string $name The name of the named section.
+   */
+  public function delNamedSession(string $name): void;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
    * Returns stateful double submit token to prevent CSRF attacks.
    *
    * @return string
@@ -22,6 +46,17 @@ interface Session
    * @return int
    */
   public function getLanId(): int;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Gets a named section of the session. If such named section does not yet exists an empty array is returned.
+   *
+   * @param string $name The name of the named section.
+   * @param int    $mode The locke mode.
+   *
+   * @return array
+   */
+  public function getNamedSection(string $name, int $mode): array;
 
   //--------------------------------------------------------------------------------------------------------------------
   /**
@@ -80,6 +115,21 @@ interface Session
    * @return void
    */
   public function logout(): void;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Stores a named section of the session.
+   *
+   * Normally will return true. However, in NAMED_FIRST_COME_FIRST_SERVED mode will return false when the named section
+   * of the session could not be updated (due to some other request has updated the named section before).
+   *
+   * @param string     $name  The name of the named section.
+   * @param array|null $value The value of the named section of the session. A null value will delete the named section
+   *                          of the session.
+   *
+   * @return bool
+   */
+  public function putNamedSection(string $name, ?array $value): bool;
 
   //--------------------------------------------------------------------------------------------------------------------
   /**
