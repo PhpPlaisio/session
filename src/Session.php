ad8e9c135b902521ab05d5e0b7b8b7697817d700
Paul Water <p.r.water@setbased.nl>
2020-04-19 02:53:23 +0200

    Added method destroySessions.

--- src/Session.php
+++ src/Session.php
@@ -26,6 +26,14 @@ interface Session
 
   //--------------------------------------------------------------------------------------------------------------------
   /**
+   * Destroys sessions of the current user.
+   *
+   * @param bool $current If true all session will be destroyed. Otherwise, all other sessions will be destroyed.
+   */
+  public function destroySessions(bool $current = false): void;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
    * Returns stateful double submit token to prevent CSRF attacks.
    *
    * @return string
@@ -51,12 +59,13 @@ interface Session
    * Returns a reference to the data of a named section of the session.
    *
    * If the named section does not yet exists a reference to null is returned. Only named sections opened in shared
-   * and exclusive mode will be saved by @see save.
+   * and exclusive mode will be saved by @param string $name The name of the named section.
    *
-   * @param string $name The name of the named section.
-   * @param int    $mode The mode for getting the named section.
+   * @param int $mode The mode for getting the named section.
    *
    * @return mixed
+   *
+   * @see   save.
    *
    * @since 1.0.0
    * @api
